f693f0b0a44eb59f8635b017a924e9dce79bf2b0
Francesco Ilario <filario@redhat.com>
2024-03-11 16:10:03 +0100

    update install scripts
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/install_workspaces.sh
+++ hack/install_workspaces.sh
@@ -5,9 +5,9 @@ set -e
 export QUAY_NAMESPACE=${QUAY_NAMESPACE:-workspaces}
 
 f="$(pwd)"
-[[ "${f}" == "/tmp/*" ]] || f=$(mktemp --directory /tmp/workspaces-demo.XXXX)
+[[ "${f}" == "/tmp/*" ]] || {
+  f=$(mktemp --directory /tmp/workspaces-demo.XXXX)
+  cp -r hack/ operator/ e2e/ server/ ingress/ "$f" 
+}
 
-cp -r hack/ operator/ e2e/ server/ "$f"
-cd "$f" 
-
-make -C e2e prepare
+make -C "$f/e2e" prepare
